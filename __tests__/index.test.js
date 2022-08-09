2c5caf3b922f56c5d81fe8ccb3bd12250598e7a2
BZaragoza <brianzaragoza2010@gmail.com>
2022-08-08 20:58:20 -0600

    feat: add node deps and linting to existing workflow

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,13 @@
-import request from 'supertest'
-import app from '../index'
+import request from "supertest";
+import app from "../index";
 
 describe("Get route", () => {
   it("page should return hello world", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
     expect(res.body.msg).toEqual("Hello world");
-  });
-});
+  })
+})
 
 afterAll(async () => {
   await app.close()
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  res.status(200).json({msg: "Hello world"});
-});
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'Hello world' })
+})
 
 export default app.listen(process.env.PORT || 4000, () =>
-  console.log(`Running on http://localhost:4000`)
-);
+  console.log('Running on http://localhost:4000')
+)
